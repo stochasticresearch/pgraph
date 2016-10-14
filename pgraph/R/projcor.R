a62fe7bd26e04c018fef9a615b5308fd577e99a2
Kiran Karra <kiran.karra@gmail.com>
2016-10-14 11:23:40 -0400

    added projcor to repo

---
+++ pgraph/R/projcor.R
@@ -0,0 +1,55 @@
+# Modified from projcov.R by Kiran Karra (kiran.karra@gmail.com) to return the 
+# distance correlation of the residuals, which should be a rough measure of conditional
+# dependence.
+
+#' Calculate the Projected Covariance of Two Vectors
+#'
+#' \code{projcov} calculate the projected distance covariance of two vectors given
+#' common factors.
+#'
+#' @export
+#' @importFrom stats cor
+#' @importFrom stats predict
+#' @importFrom stats cov
+#' @importFrom energy dcov.test
+#' @importFrom glmnet cv.glmnet
+#' @importFrom splines ns
+#' @importFrom SAM samQL
+#' @importFrom stats coef
+#' @importFrom stats lm.fit
+#' @param x first vector
+#' @param y second vector
+#' @param b factor matrix
+#' @param method projection method. Default = 'linear'.
+#' @param one.SE whether to use the 1se rule for glmnet. Default = TRUE.
+#' @param refit whether to refit the selected model. Default = TRUE.
+#' @param randSeed the random seed for the program. Default = 0.
+#' @return a list.
+#' \item{test.pearson}{pearson correlection test statistic}
+#' \item{test.dcov}{distance covariance test statistic}
+#' \item{xeps}{residual of  projection of x on b}
+#' \item{yeps}{residual of  projection of y on b}
+#' @seealso \code{\link{greg}}, \code{\link{roc}}, \code{\link{pgraph}}
+#' @examples
+#' library(splines)
+#' set.seed(0)
+#' K = 3
+#' n = 100
+#' b = matrix(rnorm(K*n),n,K)
+#' bx = 1:3
+#' by = c(1,2,2)
+#' x = b%*%bx+rnorm(n)
+#' y = b%*%by+rnorm(n)
+#' fit1 = projcov(x, y, b, method = "lasso")
+#' fit2 = projcov(x, y, b, method = "sam")
+projcor <- function(x, y, b, method = c("lasso","sam"), one.SE = TRUE, refit = TRUE, randSeed = 0){
+  method = match.arg(method)
+  set.seed(randSeed)
+  xeps = projcore(x, b, method = method, one.SE = one.SE, refit = refit, randSeed = randSeed)
+  yeps = projcore(y, b, method = method, one.SE = one.SE, refit = refit, randSeed = randSeed)
+
+  
+  projcor = energy::dcor(xeps,yeps)
+
+return(list=list(projcor.statistic=projcor, xeps=xeps,yeps=yeps))
+}
